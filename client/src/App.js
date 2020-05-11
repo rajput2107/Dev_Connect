3b0ef2c87fa43534e4f8cb164c59bd2e90880b10
rajput2107 <pramodrana2107@gmail.com>
2020-05-11 15:56:37 +0530

    Deploying

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,10 @@ import './App.css';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
 import {loadUser} from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
-// import CreateProfile from './components/profileForms/CreateProfile';
-// import EditProfile from './components/profileForms/EditProfile';
-import ProfileForm from './components/profileForms/ProfileForm';
-import AddExperience from './components/profileForms/AddExperience';
-import AddEducation from './components/profileForms/AddEducation';
-import Profiles from './components/profiles/Profiles';
-import Profile from '../src/components/profile/Profile';
-import Posts from '../src/components/posts/Posts';
-import Post from '../src/components/post/Post';
+import Routes from './components/routing/Routes';
+
 
 
 //Redux
@@ -44,23 +32,10 @@ const App= ()=>{
   <Router>
     <Fragment>
       <Navbar />
+      <Switch>
       <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Alert />
-        <Switch>
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/profiles' component={Profiles} />
-        <Route exact path='/profile/:id' component={Profile} />
-        <PrivateRoute exact path='/dashboard' component={Dashboard} />   {/*Force user to login before going to dashboard */}
-        <PrivateRoute exact path='/create-profile' component={ProfileForm} /> 
-        <PrivateRoute exact path='/edit-profile' component={ProfileForm} /> 
-        <PrivateRoute exact path='/add-experience' component={AddExperience} /> 
-        <PrivateRoute exact path='/add-education' component={AddEducation} /> 
-        <PrivateRoute exact path='/posts' component={Posts} /> 
-        <PrivateRoute exact path='/posts/:id' component={Post} /> 
-        </Switch>
-      </section>    
+      <Route component={Routes} />
+      </Switch>
     </Fragment> 
   </Router>
   </Provider>
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 
 // Connect DB
@@ -9,15 +10,27 @@ connectDB();
 // Init Middleware(BodyParser)
 app.use(express.json({extended: false}));
 
-app.get('/',(req,res)=>{
-    res.send('Running');
-});
+// app.get('/',(req,res)=>{
+//     res.send('Running');
+// });
 
 // Define Routes
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/posts',require('./routes/api/posts'));
 app.use('/api/profile',require('./routes/api/profile'));
+
+
+
+// Serve Static Assets in Production
+if(process.env.NODE_ENV === 'production'){
+    //Set Static Folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 
 const PORT = process.env.PORT || 5000;
